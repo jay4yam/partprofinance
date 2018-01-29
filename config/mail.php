04a7346419f1e31f5874d134dcbbdf420d85dda5
jay4yam <jay.ayamee@gmail.com>
2018-01-29 09:41:38 +0100

    Update Calendar v2

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'partprofinance@partprofinance.ovh'),
+        'name' => env('MAIL_FROM_NAME', 'PartProFinance'),
     ],
 
     /*
