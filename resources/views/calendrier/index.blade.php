408593da80889cc244e5e499714113519ff19328
jay4yam <jay.ayamee@gmail.com>
2018-02-03 18:28:48 +0100

    change calendar col

--- resources/views/calendrier/index.blade.php
+++ resources/views/calendrier/index.blade.php
@@ -7,68 +7,8 @@
 <!-- Main content -->
 <section class="content">
     <div class="row">
-        <div class="col-md-3">
-            <div class="box box-solid">
-                <div class="box-header with-border">
-                    <h4 class="box-title">Draggable Events</h4>
-                </div>
-                <div class="box-body">
-                    <!-- the events -->
-                    <div id="external-events">
-                        <div class="external-event bg-green">Lunch</div>
-                        <div class="external-event bg-yellow">Go home</div>
-                        <div class="external-event bg-aqua">Do homework</div>
-                        <div class="external-event bg-light-blue">Work on UI design</div>
-                        <div class="external-event bg-red">Sleep tight</div>
-                        <div class="checkbox">
-                            <label for="drop-remove">
-                                <input type="checkbox" id="drop-remove">
-                                remove after drop
-                            </label>
-                        </div>
-                    </div>
-                </div>
-                <!-- /.box-body -->
-            </div>
-            <!-- /. box -->
-            <div class="box box-solid">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Create Event</h3>
-                </div>
-                <div class="box-body">
-                    <div class="btn-group" style="width: 100%; margin-bottom: 10px;">
-                        <!--<button type="button" id="color-chooser-btn" class="btn btn-info btn-block dropdown-toggle" data-toggle="dropdown">Color <span class="caret"></span></button>-->
-                        <ul class="fc-color-picker" id="color-chooser">
-                            <li><a class="text-aqua" href="#"><i class="fa fa-square"></i></a></li>
-                            <li><a class="text-blue" href="#"><i class="fa fa-square"></i></a></li>
-                            <li><a class="text-light-blue" href="#"><i class="fa fa-square"></i></a></li>
-                            <li><a class="text-teal" href="#"><i class="fa fa-square"></i></a></li>
-                            <li><a class="text-yellow" href="#"><i class="fa fa-square"></i></a></li>
-                            <li><a class="text-orange" href="#"><i class="fa fa-square"></i></a></li>
-                            <li><a class="text-green" href="#"><i class="fa fa-square"></i></a></li>
-                            <li><a class="text-lime" href="#"><i class="fa fa-square"></i></a></li>
-                            <li><a class="text-red" href="#"><i class="fa fa-square"></i></a></li>
-                            <li><a class="text-purple" href="#"><i class="fa fa-square"></i></a></li>
-                            <li><a class="text-fuchsia" href="#"><i class="fa fa-square"></i></a></li>
-                            <li><a class="text-muted" href="#"><i class="fa fa-square"></i></a></li>
-                            <li><a class="text-navy" href="#"><i class="fa fa-square"></i></a></li>
-                        </ul>
-                    </div>
-                    <!-- /btn-group -->
-                    <div class="input-group">
-                        <input id="new-event" type="text" class="form-control" placeholder="Event Title">
-
-                        <div class="input-group-btn">
-                            <button id="add-new-event" type="button" class="btn btn-primary btn-flat">Add</button>
-                        </div>
-                        <!-- /btn-group -->
-                    </div>
-                    <!-- /input-group -->
-                </div>
-            </div>
-        </div>
         <!-- /.col -->
-        <div class="col-md-9">
+        <div class="col-md-12">
             <div class="box box-primary">
                 <div class="box-body no-padding">
                     <!-- THE CALENDAR -->
@@ -93,32 +33,6 @@
     <!-- Page specific script -->
     <script>
         $(function () {
-
-            /* initialize the external events
-             -----------------------------------------------------------------*/
-            function init_events(ele) {
-                ele.each(function () {
-
-                    // create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
-                    // it doesn't need to have a start or end
-                    var eventObject = {
-                        title: $.trim($(this).text()) // use the element's text as the event title
-                    };
-
-                    // store the Event Object in the DOM element so we can get to it later
-                    $(this).data('eventObject', eventObject);
-
-                    // make the event draggable using jQuery UI
-                    $(this).draggable({
-                        zIndex        : 1070,
-                        revert        : true, // will cause the event to go back to its
-                        revertDuration: 0  //  original position after the drag
-                    })
-
-                })
-            }
-
-            init_events( $('#external-events div.external-event') );
 
             /* initialize the calendar
              -----------------------------------------------------------------*/
@@ -174,42 +88,6 @@
 
                 }
             });
-
-            /* ADDING EVENTS */
-            var currColor = '#3c8dbc'; //Red by default
-            //Color chooser button
-            var colorChooser = $('#color-chooser-btn');
-            $('#color-chooser > li > a').click(function (e) {
-                e.preventDefault();
-                //Save color
-                currColor = $(this).css('color');
-                //Add color effect to button
-                $('#add-new-event').css({ 'background-color': currColor, 'border-color': currColor });
-            });
-            $('#add-new-event').click(function (e) {
-                e.preventDefault();
-                //Get value and make sure it is not null
-                var val = $('#new-event').val();
-                if (val.length == 0) {
-                    return
-                }
-
-                //Create events
-                var event = $('<div />');
-                event.css({
-                    'background-color': currColor,
-                    'border-color'    : currColor,
-                    'color'           : '#fff'
-                }).addClass('external-event');
-                event.html(val);
-                $('#external-events').prepend(event);
-
-                //Add draggable funtionality
-                init_events(event);
-
-                //Remove event from text input
-                $('#new-event').val('');
-            })
         })
     </script>
 @endsection
