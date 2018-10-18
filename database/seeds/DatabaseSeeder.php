e3c17da07def1a249d62e6e35cf009acfc848466
jay4yam <jay.ayamee@gmail.com>
2018-10-18 20:47:42 +0200

    dashboard personnalisé

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,11 +18,11 @@ class DatabaseSeeder extends Seeder
         $this->call(PortetUserSeeder::class);
         */
 
-        $this->call(SuperUserSeeder::class);
-        $this->call(SalesUserSeeder::class);
-        $this->call(BanqueSeeder::class);
-        //$this->call(InsertOldTempProspectData::class);
-        //$this->call(InsertOldProspectData::class);
-        //$this->call(InsertOldDossierData::class);
+        //$this->call(SuperUserSeeder::class);
+        //$this->call(SalesUserSeeder::class);
+        //$this->call(BanqueSeeder::class);
+        $this->call(InsertOldTempProspectData::class);
+        $this->call(InsertOldProspectData::class);
+        $this->call(InsertOldDossierData::class);
     }
 }
