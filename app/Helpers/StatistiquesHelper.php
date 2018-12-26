a94501f7eb27f8298a4e8bf4c3bfc3c9e3d5c1f9
jay4yam <jay.ayamee@gmail.com>
2018-12-26 16:42:25 +0100

    update sale () stats

--- app/Helpers/StatistiquesHelper.php
+++ app/Helpers/StatistiquesHelper.php
@@ -40,11 +40,15 @@ class StatistiquesHelper
 
     public function getProspectForMonthAndYear($month, $year)
     {
-        $tempProspect = new TempProspect();
-        $tempProspectOftheMonth = $tempProspect->countUserWithDate($month, $year)->count();
+        $tempProspectOftheMonth = Cache::remember('TempProspectForAdmin', 100, function () use($month, $year){
+            $tempProspect = new TempProspect();
+            return $tempProspect->countUserWithDate($month, $year)->count();
+        });
 
-        $prospect = new Prospect();
-        $prospectOfTheMonth = $prospect->countUserWithDate($month, $year)->count();
+        $prospectOfTheMonth = Cache::remember('ProspectForAdmin', 100, function () use ($month, $year){
+            $prospect = new Prospect();
+            return $prospect->countUserWithDate($month, $year)->count();
+        });
 
         return $tempProspectOftheMonth + $prospectOfTheMonth;
     }
